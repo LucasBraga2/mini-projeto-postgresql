5ae74eb9a40fd828fcc18167e2ae50b9b6506f5c
Lucas Braga <130006732+LucasBraga2@users.noreply.github.com>
2025-11-09 23:08:20 -0300

    teste token

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -66,7 +66,7 @@ class AuthService {
     const token = jwt.sign(
       { id: user.id, email: user.email }, 
       secret,
-      { expiresIn: '30s' }
+      { expiresIn: '1m' }
     );
 
     console.log(`[AuthService] Usuário ${email} logado com sucesso.`);
